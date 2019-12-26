57d7d593a4355c7a886f52d3334ccb464406e390
jonmartz <jonmartz91@gmail.com>
2019-12-26 10:25:28 +0200

    Initial commit

---
+++ test/TreeTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class TreeTest {
+}
