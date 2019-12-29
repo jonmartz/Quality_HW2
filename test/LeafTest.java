b15cae0b5c46cadf4ccc0e2f9db8030ed367cc60
Ofir Gruner <ofirgruner@gmail.com>
2019-12-29 21:49:08 +0200

    Tests for Leaf and Tree

--- test/LeafTest.java
+++ test/LeafTest.java
@@ -3,8 +3,124 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
 public class LeafTest {
+    private Random randomNum;
+    private final int highest = 101;
+    //private String name = "test";
+    //private int size = 7;
+    //private int diskSpace = 10;
+    //private FileSystem fileSystem = new FileSystem(diskSpace);
+
+    @Before
+    public void beforeTests() {
+        randomNum = new Random();
+    }
+
+    @Test
+    //Test for checking simple creation
+    public void checkCreation() throws OutOfSpaceException{
+        int size = this.randomNum.nextInt(highest);
+        FileSystem.fileStorage = new Space(size+1);
+        Leaf leaf = new Leaf("nameOfLeaf",size);
+        FileSystem.fileStorage = null;
+    }
+
+    @Test(expected = OutOfSpaceException.class)
+    //Test for checking invalid creation
+    public void checkInvalidCreation() throws OutOfSpaceException {
+        int size = this.randomNum.nextInt(highest);
+        FileSystem.fileStorage = new Space(size+1);
+        Leaf leaf = new Leaf("nameOfLeaf",size*10);
+        FileSystem.fileStorage = null;
+    }
+
+    @Test
+    //Test for checking if names match between received name and existing name of the object Leaf.
+    public void checkName() {
+        Leaf leaf = null;
+        int size = randomNum.nextInt(highest);
+        FileSystem.fileStorage = new Space(size+1);
+        String name = "nameOfLeaf";
+        try {
+            leaf = new Leaf(name,size);
+        }
+        catch (OutOfSpaceException e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+        assertEquals(leaf.name,name);
+        FileSystem.fileStorage = null;
+    }
+
+    @Test
+    //Test for checking if size for the leaf been allocated as needed.
+    public void checkSize() {
+        Leaf leaf = null;
+        int size = this.randomNum.nextInt(highest);
+        FileSystem.fileStorage = new Space(size+1);
+        String name = "nameOfLeaf";
+        try {
+            leaf = new Leaf(name,size);
+        }
+        catch (OutOfSpaceException e) {
+            e.printStackTrace();
+        }
+        assertEquals(leaf.allocations.length,size);
+        for(int i = 0 ; i < size ; i++){
+            assertEquals(FileSystem.fileStorage.getAlloc()[leaf.allocations[i]],leaf);
+        }
+        FileSystem.fileStorage = null;
+    }
+
+    @Test
+    //Test for checking the leaf path.
+    public void checkLeafPath()
+    {
+        Leaf leaf = null;
+        int size = this.randomNum.nextInt(highest);
+        int leafNodeDepth = this.randomNum.nextInt(highest);
+        FileSystem.fileStorage = new Space(size+1);
+        Node nodeBefore = leaf;
+        String name = "nameOfLeaf";
+        try {
+            leaf = new Leaf(name,size);
+        }
+        catch (OutOfSpaceException e) {
+            e.printStackTrace();
+        }
+        leaf.depth = leafNodeDepth+1;
+        List<Tree> forest = new ArrayList<>();
+        Tree tree = null;
+        String treeName = "nameOfTree";
+        for(int i = 0 ; i < leafNodeDepth ; i++){
+            tree = new Tree(treeName + ": " + i);
+            nodeBefore.parent = tree;
+            tree.depth = nodeBefore.depth-1;
+            nodeBefore = tree;
+            if(nodeBefore instanceof Tree) {
+                forest.add(0, (Tree)nodeBefore);
+            }
+        }
+        String[] leafPath = leaf.getPath();//From Node class function
+        assertEquals(leafPath.length,leafNodeDepth+1);
+        assertEquals(leafPath[leafPath.length-1],name);
+        for(int j = 0 ; j < forest.size() ; j++){
+            assertEquals(forest.get(j).name,leafPath[j]);
+        }
+        FileSystem.fileStorage = null;
+    }
+
+    @After
+    public void clear()
+    {
+        FileSystem.fileStorage = null;
+    }
 }
+
--- test/TreeTest.java
+++ test/TreeTest.java
@@ -1,10 +1,117 @@
 import org.junit.*;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
 public class TreeTest {
+    private Random randomNum;
+    private final int highest = 101;
+
+    @Before
+    public void beforeTests() {
+        randomNum = new Random();
+    }
+
+    @Test
+    //Test for checking if names match between received name and existing name of the object Tree.
+    public void checkName() {
+        String treeName = "nameOfTree";
+        Tree tree = new Tree(treeName);
+        assertEquals(tree.name, treeName);
+    }
+
+    @Test
+    //Test for getChildByName when Child exists:
+    public void checkExistGetChildByName()
+    {
+        Node child1=null;
+        Node child2=null;
+        String treeName = "nameOfTree";
+        int childrenCount = randomNum.nextInt(highest);;
+        Tree tree = new Tree(treeName);
+        List<Tree> childrenToCheck = new ArrayList<>();
+        for(int i=0 ; i < childrenCount ; i++){
+            childrenToCheck.add(new Tree(treeName+i));
+        }
+        for(int j=0 ; j < childrenToCheck.size() ; j++){
+            child1 = childrenToCheck.get(j);
+            tree.children.put(child1.name,child1);
+        }
+        for(int k=0 ; k < childrenToCheck.size() ; k++){
+            child1 = childrenToCheck.get(k);
+            child2 = tree.GetChildByName(child1.name);
+            assertEquals(child1,child2);
+        }
+        FileSystem.fileStorage = null;
+    }
+
+    @Test
+    //Test for getChildByName when Child don't exists:
+    public void checkNotExistGetChildByName()
+    {
+        Tree childTree;
+        Node childNode;
+        String cName;
+        String treeName = "nameOfTree";
+        List<Tree> childrenToCheck = new ArrayList<>();
+        int childrenCount = randomNum.nextInt(highest);;
+        Tree tree = new Tree(treeName);
+        List<String> childrenNameToCheck = new ArrayList<>();
+        for(int i = 0 ; i < childrenCount ; i++){
+            childrenNameToCheck.add(treeName + i);
+        }
+        for(int j=0 ; j < childrenNameToCheck.size() ; j++){
+            cName = childrenNameToCheck.get(j);
+            assertTrue(tree.children.get(cName) == null);
+            childTree = tree.GetChildByName(cName);
+            assertEquals(childTree.depth,tree.depth+1);
+            assertEquals(tree.children.get(cName),childTree);
+            childrenToCheck.add(childTree);
+        }
+        for(int k = 0 ; k < childrenToCheck.size() ; k++){
+            childTree = childrenToCheck.get(k);
+            childNode = tree.GetChildByName(childTree.name);
+            assertEquals(childTree,childNode);
+        }
+        FileSystem.fileStorage = null;
+    }
+
+    @Test
+    //Test for checking the tree path.
+    public void checkTreePath() {
+        int treeNodeDepth = randomNum.nextInt(highest);
+        String treeNameTest = "nameOfTreeTest";
+        String treeName = "nameOfTree";
+        Tree treeTest = new Tree(treeNameTest);
+        Tree tree;
+        Node nodeBefore = treeTest;
+        treeTest.depth = treeNodeDepth + 1;
+        List<Tree> forest = new ArrayList<>();
+        for (int i = 0; i < treeNodeDepth; i++) {
+            tree = new Tree(treeName + " "+i);
+            nodeBefore.parent = tree;
+            tree.depth = nodeBefore.depth - 1;
+            nodeBefore = tree;
+            if (nodeBefore instanceof Tree) {
+                forest.add(0, (Tree)nodeBefore);
+            }
+        }
+        String[] treePath = treeTest.getPath();
+        assertEquals(treePath.length, treeNodeDepth + 1);
+        assertEquals(treePath[treePath.length - 1], treeNameTest);
+        for (int j = 0; j < forest.size(); j++) {
+            assertEquals(forest.get(j).name, treePath[j]);
+        }
+        FileSystem.fileStorage = null;
+    }
+
+    @After
+    public void clear()
+    {
+        FileSystem.fileStorage = null;
+    }
 }
